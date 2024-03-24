ec41b1cae1f67fe2cd9b206eb084408b1a34b690
kolodani <danielvinzia@gmail.com>
2024-03-24 17:24:29 -0300

    pruebas en casa

--- 000-Recursos_zips/HelloController.java
+++ 000-Recursos_zips/HelloController.java
@@ -1,17 +1,17 @@
-package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestController;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args){
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
     public class HelloController{
         @GetMapping
