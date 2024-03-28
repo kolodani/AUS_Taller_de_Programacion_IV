014fda194e1543ba0fdccfa1437e1c9ac019825a
Daniel Alberto Vinzia <danielvinzia@gmail.com>
2024-03-28 19:46:04 -0300

    avanzamos con el proyecto en clase

--- 001-trabajo_en_clases/001-Clase01/demo/src/main/java/com/example/demo/HelloController.java
+++ 001-trabajo_en_clases/001-Clase01/demo/src/main/java/com/example/demo/HelloController.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("api")
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
+    @Autowired
+    private Hola h;
+
+    @GetMapping("hello")
     public String hello() {
-        return "Hello, World!";
+        return h.hola();
     }
 }
+
+// http://localhost:8080/api/hello
