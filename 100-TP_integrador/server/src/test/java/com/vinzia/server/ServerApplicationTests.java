b7e6391b6be1db8473eaba431815858076e5fc3d
kolodani <danielvinzia@gmail.com>
2024-06-13 14:42:11 -0300

    TP_integrador, inicio

---
+++ 100-TP_integrador/server/src/test/java/com/vinzia/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinzia.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
