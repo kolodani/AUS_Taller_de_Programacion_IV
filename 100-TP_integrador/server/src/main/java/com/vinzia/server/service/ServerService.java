4837a29dc61c63250f445a1ab0ea2ec2658092db
kolodani <danielvinzia@gmail.com>
2024-06-13 16:34:54 -0300

    TP_integrador, Service Implementation

--- 100-TP_integrador/server/src/main/java/com/vinzia/server/service/ServerService.java
+++ 100-TP_integrador/server/src/main/java/com/vinzia/server/service/ServerService.java
@@ -2,11 +2,12 @@ package com.vinzia.server.service;
 
 import com.vinzia.server.model.Server;
 
+import java.io.IOException;
 import java.util.Collection;
 
 public interface ServerService {
     Server create(Server server);
-    Server ping(String ipAddress);
+    Server ping(String ipAddress) throws IOException;
     Collection<Server> list(int limit);
     Server get(Long id);
     Server update(Server server);
--- 100-TP_integrador/server/src/main/java/com/vinzia/server/service/implementation/ServerServiceImplementation.java
+++ 100-TP_integrador/server/src/main/java/com/vinzia/server/service/implementation/ServerServiceImplementation.java
@@ -1,20 +1,43 @@
 package com.vinzia.server.service.implementation;
 
 import com.vinzia.server.model.Server;
+import com.vinzia.server.repo.ServerRepo;
 import com.vinzia.server.service.ServerService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Collection;
 import java.util.List;
 
+import static com.vinzia.server.enumeration.Status.SERVER_DOWN;
+import static com.vinzia.server.enumeration.Status.SERVER_UP;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
 public class ServerServiceImplementation implements ServerService {
+    private final ServerRepo serverRepo;
+
     @Override
     public Server create(Server server) {
-        return null;
+        log.info("Saving new server: {}", server.getName());
+        server.setImageUrl(setServerImageUrl());
+        return serverRepo.save(server);
     }
 
     @Override
-    public Server ping(String ipAddress) {
-        return null;
+    public Server ping(String ipAddress) throws IOException {
+        log.info("Pinging server: {}", ipAddress);
+        Server server = serverRepo.findByIpAddress(ipAddress);
+        InetAddress address = InetAddress.getByName(ipAddress);
+        server.setStatus(address.isReachable(10000) ? SERVER_UP : SERVER_DOWN);
+        serverRepo.save(server);
+        return server;
     }
 
     @Override
@@ -34,6 +57,10 @@ public class ServerServiceImplementation implements ServerService {
 
     @Override
     public Boolean delete(Long id) {
+        return null;
+    }
+
+    private String setServerImageUrl() {
         return null;
     }
 }
