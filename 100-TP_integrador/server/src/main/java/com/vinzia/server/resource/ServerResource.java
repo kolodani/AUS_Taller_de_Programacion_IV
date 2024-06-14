fa6578562098ecbf62aca11ebccbab0f24a8f0c7
kolodani <danielvinzia@gmail.com>
2024-06-14 14:50:53 -0300

    TP_integrador, HTTP Resource part 1

---
+++ 100-TP_integrador/server/src/main/java/com/vinzia/server/resource/ServerResource.java
@@ -0,0 +1,34 @@
+package com.vinzia.server.resource;
+
+import com.vinzia.server.model.Response;
+import com.vinzia.server.service.implementation.ServerServiceImplementation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+import static java.time.LocalDateTime.now;
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequestMapping("/server")
+@RequiredArgsConstructor
+public class ServerResource {
+    private final ServerServiceImplementation serverService;
+
+    @GetMapping("/List")
+    public ResponseEntity<Response> getServers() {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(Map.of("servers", serverService.list(30)))
+                        .message("Servers retrieved")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+}
