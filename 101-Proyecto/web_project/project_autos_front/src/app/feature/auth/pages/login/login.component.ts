afee18ee1409a0d3b2aa99bec1bdfd5aa750e3e3
kolodani <danielvinzia@gmail.com>
2024-09-19 18:11:14 -0300

    falta completar formularios

--- 101-Proyecto/web_project/project_autos_front/src/app/feature/auth/pages/login/login.component.ts
+++ 101-Proyecto/web_project/project_autos_front/src/app/feature/auth/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  constructor(private router: Router) {
+
+  }
+
+  public signIn(): void {
+    alert("Me presionaron");
+  }
+
+  public signUp(): void {
+    this.router.navigateByUrl("autenticacion/registro");
+  }
 
 }
